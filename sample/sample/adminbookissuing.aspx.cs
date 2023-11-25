9ceb370e90c9a69134f020fc3e7336e124f3904e
Your Name <you@example.com>
2023-11-25 17:16:02 +0600

    First Updated

---
+++ sample/sample/adminbookissuing.aspx.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace sample
+{
+	public partial class adminbookissuing : System.Web.UI.Page
+	{
+		string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			GridView1.DataBind();
+        
+		}
+		//search button
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+			getNames();
+
+		}
+		//issue button
+		protected void Button2_Click(object sender, EventArgs e)
+		{
+			if (checkIfBookExist() && checkIfUserExist())
+			{
+
+				if (checkIfIssueEntryExist())
+				{
+					Response.Write("<script>alert('This Member already has this book');</script>");
+				}
+				else
+				{
+					issueBook();
+				}
+
+			}
+			else
+			{
+				Response.Write("<script>alert('Wrong Book ID or Member ID');</script>");
+			}
+
+		}
+		//return button
+		protected void Button4_Click(object sender, EventArgs e)
+		{
+			if (checkIfBookExist() && checkIfUserExist())
+			{
+
+				if (checkIfIssueEntryExist())
+				{
+					returnBook();
+				}
+				else
+				{
+					Response.Write("<script>alert('This Entry does not exist');</script>");
+				}
+
+			}
+			else
+			{
+				Response.Write("<script>alert('Wrong Book ID or user ID');</script>");
+			}
+
+		}
+
+		//user defined method
+		//return button work
+		void returnBook()
+		{
+			try
+			{
+				SqlConnection con = new SqlConnection(strcon);
+				if (con.State == ConnectionState.Closed)
+				{
+					con.Open();
+				}
+
+
+				SqlCommand cmd = new SqlCommand("Delete from book_issue_tbl WHERE book_id='" + TextBox1.Text.Trim() + "' AND user_id='" + TextBox2.Text.Trim() + "'", con);
+				int result = cmd.ExecuteNonQuery();
+
+				if (result > 0)
+				{
+
+					cmd = new SqlCommand("update book_master_table set current_stock = current_stock+1 WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+					cmd.ExecuteNonQuery();
+					con.Close();
+
+					Response.Write("<script>alert('Book Returned Successfully');</script>");
+					GridView1.DataBind();
+
+					con.Close();
+
+				}
+				else
+				{
+					Response.Write("<script>alert('Error - Invalid details');</script>");
+				}
+
+			}
+			catch (Exception ex)
+			{
+				Response.Write("<script>alert('" + ex.Message + "');</script>");
+			}
+		}
+		//get name method,search button work
+		void getNames()
+		{
+			try
+			{
+				SqlConnection con = new SqlConnection(strcon);
+				if (con.State == ConnectionState.Closed)
+				{
+					con.Open();
+				}
+				SqlCommand cmd = new SqlCommand("select book_name from book_master_table WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+				SqlDataAdapter da = new SqlDataAdapter(cmd);
+				DataTable dt = new DataTable();
+				da.Fill(dt);
+				if (dt.Rows.Count >= 1)
+				{
+					TextBox4.Text = dt.Rows[0]["book_name"].ToString();
+				}
+				else
+				{
+					Response.Write("<script>alert('Wrong Book ID');</script>");
+				}
+
+				cmd = new SqlCommand("select full_name from user_master_tbl WHERE user_id='" + TextBox2.Text.Trim() + "'", con);
+				da = new SqlDataAdapter(cmd);
+				dt = new DataTable();
+				da.Fill(dt);
+				if (dt.Rows.Count >= 1)
+				{
+					TextBox3.Text = dt.Rows[0]["full_name"].ToString();
+				}
+				else
+				{
+					Response.Write("<script>alert('Wrong User ID');</script>");
+				}
+
+
+			}
+			catch (Exception ex)
+			{
+
+			}
+		}
+
+		
+		//book issue method
+		void issueBook()
+		{
+			try
+			{
+				SqlConnection con = new SqlConnection(strcon);
+				if (con.State == ConnectionState.Closed)
+				{
+					con.Open();
+				}
+
+				SqlCommand cmd = new SqlCommand("INSERT INTO book_issue_tbl(user_id,user_name,book_id,book_name,start_date,end_date) values(@user_id,@user_name,@book_id,@book_name,@start_date,@end_date)", con);
+
+				cmd.Parameters.AddWithValue("@user_id", TextBox2.Text.Trim());
+				cmd.Parameters.AddWithValue("@user_name", TextBox3.Text.Trim());
+				cmd.Parameters.AddWithValue("@book_id", TextBox1.Text.Trim());
+				cmd.Parameters.AddWithValue("@book_name", TextBox4.Text.Trim());
+				cmd.Parameters.AddWithValue("@start_date", TextBox5.Text.Trim());
+				cmd.Parameters.AddWithValue("@end_date", TextBox6.Text.Trim());
+
+				cmd.ExecuteNonQuery();
+
+				cmd = new SqlCommand("update  book_master_table set current_stock = current_stock-1 WHERE book_id='" + TextBox1.Text.Trim() + "'", con);
+
+				cmd.ExecuteNonQuery();
+
+				con.Close();
+				Response.Write("<script>alert('Book Issued Successfully');</script>");
+
+				GridView1.DataBind();
+			}
+			catch (Exception ex)
+			{
+				Response.Write("<script>alert('" + ex.Message + "');</script>");
+			}
+		}
+		bool checkIfBookExist()
+		{
+			try
+			{
+				SqlConnection con = new SqlConnection(strcon);
+				if (con.State == ConnectionState.Closed)
+				{
+					con.Open();
+				}
+				SqlCommand cmd = new SqlCommand("select * from book_master_table WHERE book_id='" + TextBox1.Text.Trim() + "' AND current_stock >0", con);
+				SqlDataAdapter da = new SqlDataAdapter(cmd);
+				DataTable dt = new DataTable();
+				da.Fill(dt);
+				if (dt.Rows.Count >= 1)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+
+		}
+		//check book and user existing
+		bool checkIfUserExist()
+		{
+			try
+			{
+				SqlConnection con = new SqlConnection(strcon);
+				if (con.State == ConnectionState.Closed)
+				{
+					con.Open();
+				}
+				SqlCommand cmd = new SqlCommand("select full_name from user_master_tbl WHERE user_id='" + TextBox2.Text.Trim() + "'", con);
+				SqlDataAdapter da = new SqlDataAdapter(cmd);
+				DataTable dt = new DataTable();
+				da.Fill(dt);
+				if (dt.Rows.Count >= 1)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+
+		}
+		bool checkIfIssueEntryExist()
+		{
+			try
+			{
+				SqlConnection con = new SqlConnection(strcon);
+				if (con.State == ConnectionState.Closed)
+				{
+					con.Open();
+				}
+				SqlCommand cmd = new SqlCommand("select * from book_issue_tbl WHERE user_id='" + TextBox2.Text.Trim() + "' AND book_id='" + TextBox1.Text.Trim() + "'", con);
+				SqlDataAdapter da = new SqlDataAdapter(cmd);
+				DataTable dt = new DataTable();
+				da.Fill(dt);
+				if (dt.Rows.Count >= 1)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+
+		}
+
+		protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+		{
+			try
+			{
+				if (e.Row.RowType == DataControlRowType.DataRow)
+				{
+					//Check your condition here
+					DateTime dt = Convert.ToDateTime(e.Row.Cells[5].Text);
+					DateTime today = DateTime.Today;
+					if (today > dt)
+					{
+						e.Row.BackColor = System.Drawing.Color.PaleVioletRed;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Response.Write("<script>alert('" + ex.Message + "');</script>");
+			}
+		}
+	}
+}
---
+++ sample/sample/homepage.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="homepage.aspx.cs" Inherits="sample.homepage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+   <section>
+      <div class="container-fluid d-flex justify-content-around" style="background-color: #E5E4E2;">
+          <div class="row">
+          
+              <div class="col-4 pr-4">
+         
+                  <div class="row"> 
+                      <h1 >Leaves of Grass by Walt Whitman</h1>
+                  </div>
+                  <div class="row">
+                      <h4 >A useful summary distills the source material down to its most important point to inform the reader</h4>
+                  </div>
+                   <div class="row">
+                      <p >A useful summary distills the source material down to its most important point to inform the reader</p>
+                  </div>
+                   
+
+              </div>
+               <div class="col-4">
+                  
+              </div>
+              <div class="col-4">
+                 <img class="rounded mx-auto d-block" width="420px" height="300px" src="imgs/4735.jpg"/>
+              </div>
+
+          </div>
+      </div>
+   </section>
+   <section>
+
+      <div class="container ">
+         <div class="row">
+            <div class="col-12" >
+               <center>
+                  <h2>Our Features</h2>
+                  <p><b>Our 3 Primary Features -</b></p>
+               </center>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/digital-inventory.png"/>
+                  <h4>Digital Book Inventory</h4>
+                  <p class="text-justify">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor. reprehenderit</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/search-online.png"/>
+                  <h4>Search Books</h4>
+                  <p class="text-justify">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor. reprehenderit</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/defaulters-list.png"/>
+                  <h4>Defaulter List</h4>
+                  <p class="text-justify">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor. reprehenderit</p>
+               </center>
+            </div>
+         </div>
+      </div>
+   </section>
+
+   <section>
+      
+   </section>
+   <section>
+      <div class="container">
+         <div class="row">
+            <div class="col-12">
+               <center>
+                  <h2>Our Process</h2>
+                  <p><b>Simple 3 Step Process</b></p>
+               </center>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/sign-up.png" />
+                  <h4>Sign Up</h4>
+                  <p class="text-justify">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor. reprehenderit</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/search-online.png"/>
+                  <h4>Search Books</h4>
+                  <p class="text-justify">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor. reprehenderit</p>
+               </center>
+            </div>
+            <div class="col-md-4">
+               <center>
+                  <img width="150px" src="imgs/library.png"/>
+                  <h4>Visit Us</h4>
+                  <p class="text-justify">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor. reprehenderit</p>
+               </center>
+            </div>
+         </div>
+      </div>
+   </section>
+</asp:Content>
